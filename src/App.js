faf9d4617047948810b0919b92c412dd132a175d
emma-dougherty <doughertyemma@hotmail.com>
2022-05-11 12:04:35 +0100

    adding prime number function

--- src/App.js
+++ src/App.js
@@ -6,9 +6,28 @@ function App() {
   const [inputNumber, setInputNumber] = useState(1)
   const [answer, setAnswer] = useState(1)
   
+  const isPrime = (inputNumber) => {
+
+    let isPrime = true
+
+  if (inputNumber === 1) {
+    return inputNumber
+  } 
+  else if(inputNumber > 1 ){
+    for (let i = 2; i < number; i++) {
+      if (inputNumber % i == 0) {
+          isPrime = false;
+          break;
+      }
+  }
+
+  }
+}
+
+
   useEffect (() => {
       if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
-          setAnswer("fizzbuz")
+          setAnswer("fizzbuzz")
         } else if (inputNumber % 3 === 0) {
           setAnswer("fizz")
         } else if (inputNumber % 5 === 0) {
